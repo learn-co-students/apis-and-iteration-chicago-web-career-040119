333dd59c1d2ba331e708a3cc0a8ab2a5cbe7bf75
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-04-15 15:08:00 -0500

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -8,10 +8,23 @@ def get_character_movies_from_api(character_name)
   response_hash = JSON.parse(response_string)
 
 
-  character_data = response_hash["results"].find { |data| data["name"] == character }
-  film_url = ["character_data"]
-  film_data = film_url.map { |data| JSON.parse(RestClient.get(data)) }
-  film_data
+  character_data = response_hash["results"].find { |hash| hash["name"] == character_name }
+  film_urls = character_data["films"]
+
+  film_data = film_urls.map { |https|
+    response_string = RestClient.get(https)
+    film_hash = JSON.parse(response_string)
+    film_hash
+   }
+
+
+
+   film_data
+  # film_data = film_url.map do |data|
+  #   p "value of data is"
+  #   p data
+  # end
+  # film_data
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -24,8 +37,14 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
+  list = films.map { |movie|
+   movie["title"]
+ }
+
+ puts list
   # some iteration magic and puts out the movies in a nice list
-  
+  #["results"][0]["films"].map { |data| JSON.parse(RestClient.get(data)) }
+
 end
 
 def show_character_movies(character)
