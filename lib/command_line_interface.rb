b69458472ffad3536facbcee3134296e517fa015
github username <github email address>
2019-04-19 16:30:46 -0500

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -11,5 +11,5 @@ end
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
-  puts gets.chom"Luke Skywalker"
+  gets.chomp
 end
